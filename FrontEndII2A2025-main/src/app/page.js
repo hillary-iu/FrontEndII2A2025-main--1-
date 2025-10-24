3a421763ca5fb0b1cadcd98eb2229841a921cf8e
hillary-iu <hillary.eduarda@estudante.ifro.edu.br>
2025-10-24 10:24:21 -0400

    sgsgs

--- FrontEndII2A2025-main/src/app/page.js
+++ FrontEndII2A2025-main/src/app/page.js
@@ -4,9 +4,27 @@ import styles from "./page.module.css";
 
 export default function Home() {
 
-  // let cont = 0
+  // let cont = 0;
+  const [cont, setCont] = useState(0);
   const [estilo, setEstilo] = useState('');
 
+  const subtrair = () => {
+    setCont(cont - 1);
+    console.log(cont);
+  }
+
+  const adicionar = () => {
+    setCont(cont + 1);
+    console.log(cont);
+  }
+
+  return (
+    <div>
+      <h1 className={styles.h1}>Hello World!</h1>
+      <p className={styles.paragrafo}>Meu parágrafo</p>
+      <button onClick={() => { adicionar() }}>Adicionar</button>
+      <button onClick={() => { subtrair() }}>Subtrair</button>
+      <p>Contagem {cont}</p>
 
       <button onClick={() => { setEstilo('colorido') }}>Estilo Colorido</button>
       <button onClick={() => { setEstilo('cinza') }}>Estilo Cinza</button>
